19e539299555dbe4b5248473119c6272e393cd03
Nguyen Minh Anh <xiaosasori@gmail.com>
2022-04-21 13:39:25 +0900

    update login api

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"time"
 )
 
 type jsonResponse struct {
@@ -32,9 +34,36 @@ func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 	// authenticate
 	app.infoLog.Println(creds.Username, creds.Password)
 
+	// look up the user by email
+	user, err := app.models.User.GetByEmail(creds.Username)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid username/password"))
+		return
+	}
+	// validate the user's password
+	validPassword, err := user.PasswordMatches(creds.Password)
+	if err != nil || !validPassword {
+		app.errorJSON(w, errors.New("invalid username/password"))
+		return
+	}
+	// we have a valid user, so generate a token
+	token, err := app.models.Token.GenerateToken(user.ID, 24*time.Hour)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	// save it to the db
+	err = app.models.Token.Insert(*token, *user)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 	// send back a response
-	payload.Error = false
-	payload.Message = "Signed in"
+	payload = jsonResponse{
+		Error:   false,
+		Message: "logged in",
+		Data:    envelope{"token": token},
+	}
 
 	err = app.writeJSON(w, http.StatusOK, payload)
 	if err != nil {
