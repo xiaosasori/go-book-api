1ede3f7b2bbdf9924879d6cfbad89630ee41bcd9
Nguyen Minh Anh <xiaosasori@gmail.com>
2022-04-21 18:04:57 +0900

    get book by id

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -404,3 +404,24 @@ func (app *application) EditBook(w http.ResponseWriter, r *http.Request) {
 
 	app.writeJSON(w, http.StatusAccepted, payload)
 }
+
+func (app *application) BookByID(w http.ResponseWriter, r *http.Request) {
+	bookID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	book, err := app.models.Book.GetOneById(bookID)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error: false,
+		Data:  book,
+	}
+
+	app.writeJSON(w, http.StatusOK, payload)
+}
