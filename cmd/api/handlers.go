7b08a780000b03e1c8d6a91d841f8ef6013f9aa6
Nguyen Minh Anh <xiaosasori@gmail.com>
2022-04-21 14:31:49 +0900

    logout api

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -62,11 +62,36 @@ func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 	payload = jsonResponse{
 		Error:   false,
 		Message: "logged in",
-		Data:    envelope{"token": token},
+		Data:    envelope{"token": token, "user": user},
 	}
 
 	err = app.writeJSON(w, http.StatusOK, payload)
 	if err != nil {
 		app.errorLog.Println(err)
 	}
+}
+
+func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
+	var requestPayload struct {
+		Token string `json:"token"`
+	}
+
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid json"))
+		return
+	}
+
+	err = app.models.Token.DeleteByToken(requestPayload.Token)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid json"))
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "logged out",
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
